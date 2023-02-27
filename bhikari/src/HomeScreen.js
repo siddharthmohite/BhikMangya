5b2c07fa8f61b5e9b94ef48293d9dba7fd4eb0b9
sid <sid@sids-Air.lan>
2023-02-27 01:23:16 -0800

    added buttons with sytling

---
+++ bhikari/src/HomeScreen.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './HomeScreen.css'
+
+function HomeScreen() {
+  return (
+    <div className="homescreen">
+
+      <div className="buttons">
+          <button className="spotify">
+        
+                spotify
+    
+          </button>
+
+          <button className="applemusic">
+        
+                Apple music
+    
+          </button>
+
+          <button className="youtube">
+        
+                Youtube
+    
+          </button>
+
+      </div>
+      
+
+        <img className="image_anmol"
+        src="https://i.ibb.co/NCf38y6/easymoza-LR.jpg"
+        alt=""
+            />
+    </div>
+    
+  )
+}
+
+export default HomeScreen
